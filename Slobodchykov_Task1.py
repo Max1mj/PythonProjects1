73d294295b73dd46f0cf690dcb84baba475abfb8
Jalkhet <118116354+Max1mj@users.noreply.github.com>
2023-03-09 17:50:21 +0300

    Add files via upload

--- Slobodchykov_Task1.py
+++ Slobodchykov_Task1.py
@@ -1,9 +1,9 @@
 #3.1
-print("'Introduction to programming':Task 1")
+print("'Multiparadigm programming languages' :Task 1") #название курса и номер лабы
 
-print(" Maksym Slobodchykov, IKM-221a")
+print(" Maksym Slobodchykov, Var №6")
 
-print(44*"Vladislav Ovsianikov, ","Vladislav Ovsianikov.")
+print(44*"Vladislav , ","Vladislav.")
 #3.2
 print("Show rusult:")
-print(7.8 - ( (1+ (2.1 / 3) ) /(78 + 21.3) ) )
+print(7.8 - ( (1+ (2.1 / 3) ) / (78 + 21.3) ) )
